e2e537b407bc986e2021de5bcef56f978e408bfe
Perpetua-Kabute <kabuteperpetua@gmail.com>
2021-09-27 15:49:32 +0300

    feat: set up room

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/database/ArticleDao.kt
@@ -0,0 +1,22 @@
+package com.androiddevs.mvvmnewsapp.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.androiddevs.mvvmnewsapp.data.Article
+
+//annotate so room knows that it is the interface that defines functions
+@Dao
+interface ArticleDao {
+
+    //insert or update an article
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(article: Article): Long
+
+    //return all available articles
+    @Query("SELECT * FROM articles")
+    fun getAllArticles(): LiveData<List<Article>>
+
+    //delete article
+    @Delete
+    suspend fun deleteArticle(article: Article)
+}
